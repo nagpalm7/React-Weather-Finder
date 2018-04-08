ad7956d224733ca0e83f1df6595d9d823e168f56
Mohit Nagpal <nagpalm7@gmail.com>
2018-04-08 08:14:00 +0530

    Deploying React App

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+let app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT , function(){
+  console.log('Express Server is running on ' + PORT);
+});
